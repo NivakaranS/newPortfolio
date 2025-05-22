b024a04f46f26bbe1d2d0660e8632b8f40445acc
NivakaranS <nivakaran@hotmail.com>
2025-05-22 18:46:35 +0530

    another commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="apple-mobile-web-app-title" content="Nivakaran" />
+      </head>
       <body
+      
       
         className={`${geistSans.variable} ${geistMono.variable} antialiased   overflow-x-hidden
         `}>
