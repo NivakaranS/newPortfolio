ce7c02e75ad54709aed660f378f7ec4f68af784c
NivakaranS <nivakaran@hotmail.com>
2025-05-25 18:26:03 +0530

    another commit

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -49,10 +49,12 @@ const Navigation : React.FC<ContactModelProps> = ({navSelection, onContactClick}
         <div className="flex items-center z-[50] fixed top-0 w-[100vw] justify-center">
             
             <div className="md:flex hidden flex-row  justify-between items-center px-[20px] h-[60px] 2xl:h-[55px] rounded-[8px] ring-[0.5px] ring-[#727376] w-[90vw] mt-[13px]  bg-[#373435] text-black ">
-                <div className="cursor-pointer flex flex-row items-center justify-center space-x-[5px]  text-[20px] text-white">
-                    <Image alt="test" src={Logo} height={23} width={38} />
-                    <p>NivakaranS</p>
-                </div>
+                <a href="https://www.nivakaran.dev">
+                    <div className="cursor-pointer flex flex-row items-center justify-center space-x-[5px]  text-[20px] text-white">
+                        <Image alt="test" src={Logo} height={23} width={38} />
+                        <p>NivakaranS</p>
+                    </div>
+                </a>
                 <div className="flex flex-row items-center text-[17px] justify-between text-white  w-[35%]">
                     <div onClick={onHomeClick} className={`${navSelection=='Home' ? 'text-[#FFD700] ' : ''} cursor-pointer hover:text-[#FFD700]`}>
                         <p>Home</p>
