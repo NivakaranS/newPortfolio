97325e5321dc865e64d17d82548278e17725264e
NivakaranS <nivakaran@hotmail.com>
2025-05-25 18:24:03 +0530

    another commit

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -389,7 +389,7 @@ useEffect(() => {
             
             <div className="flex flex-col space-y-[30px]  "> 
               <div className="flex flex-row items-center justify-center space-x-[20px] ">
-                <div className="w-[7px] min-h-[70vh] rounded-full bg-white">
+                <div className="w-[7px] min-h-[65vh] rounded-full bg-white">
 
                 </div>     
                 
