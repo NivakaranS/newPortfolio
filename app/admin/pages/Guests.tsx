0ba8ee3f76e6169a37722a764f7c7827b7cb6828
NivakaranS <nivakaran@hotmail.com>
2025-06-02 04:30:03 +0530

    another commit

--- app/admin/pages/Guests.tsx
+++ app/admin/pages/Guests.tsx
@@ -240,7 +240,7 @@ const BlogsManagement = () => {
                       value={blogImageLink}
                       onChange={(e) => setBlogImageLink(e.target.value)}
                       className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                      placeholder="Enter blog title"
+                      placeholder="Enter blog image link"
                     />
                   </div>
                   
